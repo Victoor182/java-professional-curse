b1be328926b439416b73a0122bc8becee319759f
Victor.Vorona <victoor182@gmail.com>
2023-08-27 19:51:18 +0200

    Add Seter

--- src/HomeWork/HomeWork1/Pensioner.java
+++ src/HomeWork/HomeWork1/Pensioner.java
@@ -4,6 +4,14 @@ public class Pensioner extends Person {
     private double pension;
 
 
+    public double getPension() {
+        return pension;
+    }
+
+    public void setPension(double pension) {
+        this.pension = pension;
+    }
+
     @Override
     public void die() {
         double earnedPension = (getAge() - 50) * pension;
--- src/HomeWork/HomeWork1/Worker.java
+++ src/HomeWork/HomeWork1/Worker.java
@@ -5,6 +5,24 @@ public class Worker extends Person {
     private int maxSalary;
 
 
+
+    public int getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(int minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public int getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(int maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+
     @Override
     public void die() {
         System.out.println("This man didn't live to see his retirement. ");
